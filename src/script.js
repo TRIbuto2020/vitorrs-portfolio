58ec51c75813c7a5474a06ebd21f3d9c5b6a522a
Vitor RS <vitor2000rs@gmail.com>
2022-12-15 12:10:30 -0300

    Bug fixes, go up button, menu animation, touchups

---
+++ src/script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const menu = document.getElementById("menu");
+    const alturaMenu = menu.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+        
+        if (posicaoAtual < alturaMenu - 20) {
+            ocultagoUp();
+        } else {
+            exibegoUp();
+        }
+    });
+
+    Array.from(document.getElementsByClassName("menu__items__item"))
+        .forEach((item, index) => {
+            item.onmouseover = () => {
+                menu.dataset.activeIndex = index;
+            }
+        });
+
+});
+
+function ocultagoUp(){
+    const goUp = document.querySelector('.goUp');
+    goUp.classList.add('goUp--is-hidden');
+};
+
+function exibegoUp(){
+    const goUp = document.querySelector('.goUp');
+    goUp.classList.remove('goUp--is-hidden');
+};
